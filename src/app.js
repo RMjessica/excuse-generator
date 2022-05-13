7b7f738e477c01db18890889867c1f0f2db5ce21
RMjessica <jeess.rm1@gmail.com>
2022-05-13 14:59:02 +0000

    finished project

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Setting up arrays
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+
+  // ramdomizing
+
+  function getRandom(arr) {
+    let r = Math.floor(Math.random() * arr.length);
+    return arr[r];
+  }
+
+  // event
+  document.getElementById("excuse").innerHTML =
+    getRandom(who) + getRandom(action) + getRandom(what) + getRandom(when);
 };
